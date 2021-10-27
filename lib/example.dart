dca92c9db43e342786e69b70c5263f0d2c76c27d
aristosh <aristo.hadisoeganda@gmail.com>
2021-10-27 13:30:03 +0700

    add examples

---
+++ lib/example.dart
@@ -0,0 +1,29 @@
+import 'package:lazy_json/lazy_json.dart';
+
+void main() {
+  final jsonObject = {
+    'response': {
+      'data': {'id': '001', 'label': 'Hello', 'number': 10}
+    }
+  };
+
+  print(jsonObject
+      .object('response')
+      .object('data')
+      .string('label', 'default value')); // prints 'Hello'
+
+  print(jsonObject
+      .object('response')
+      .object('data')
+      .string('dummyField', 'default value')); // prints 'default value'
+
+  print(jsonObject
+      .object('response')
+      .object('data')
+      .integer('number')); // prints 10
+
+  print(jsonObject
+      .object('response')
+      .object('data')
+      .integer('dummyField', 99)); // prints 10
+}
